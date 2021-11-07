1a597dc68a6ea541cd9aa518b3de730090d7dd6c
pro100 <pro100tkach@mail.ru>
2021-11-07 22:47:57 +0300

    Initial commit

---
+++ src/pro/sky/java/course2/Person.java
@@ -0,0 +1,24 @@
+package pro.sky.java.course2;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
